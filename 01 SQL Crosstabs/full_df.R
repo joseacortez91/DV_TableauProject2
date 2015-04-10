4ba59ccf57418c80bfdb99451cd1927baa81039e
Alex Pearce <alexpearce92@gmail.com>
2015-04-10 13:21:43 -0500

    Fixed table, select * works, laid out start of a crosstab

--- 01 SQL Crosstabs/full_df.R
+++ 01 SQL Crosstabs/full_df.R
@@ -1,4 +1,10 @@
 # Create general dataframe with full table data
+
 full_df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
-"select * from hospital "
+"select * from hospital"')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_jac5699', PASS='orcl_jac5699', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(full_df)
+
+# full_df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
+"select provider_name, provider_state, provider_zip, average_covered_charges, average_total_payments, average_medcare_payments,  rank() 
+OVER (PARTITION BY provider_state order by average_total_payments desc) PAYMENT_RANK FROM hospital"
 ')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_jac5699', PASS='orcl_jac5699', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(full_df)
+
