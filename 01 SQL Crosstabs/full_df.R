4711bdd551fe945761940efc1ebe99ed3d745c7f
Alex Pearce <alexpearce92@gmail.com>
2015-04-10 13:44:18 -0500

    Fixed typo and moved cross tab to separate file

--- 01 SQL Crosstabs/full_df.R
+++ 01 SQL Crosstabs/full_df.R
@@ -1,5 +1,4 @@
 # Create general dataframe with full table data
 
-full_df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
-"select * from hospital"')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_jac5699', PASS='orcl_jac5699', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(full_df)
+full_df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query="select * from hospital"')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_jac5699', PASS='orcl_jac5699', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); tbl_df(full_df)
 
