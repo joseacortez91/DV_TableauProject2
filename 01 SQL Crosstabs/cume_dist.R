3bc3c3bd173bc70dc269c4be179a03b330163cdc
ACollectionOfAtoms <hernandezdavid88@utexas.edu>
2015-04-11 11:33:33 -0500

    Created more meaningful/useful Rank and Cume tables

--- 01 SQL Crosstabs/cume_dist.R
+++ 01 SQL Crosstabs/cume_dist.R
@@ -1,10 +1,10 @@
-#Retuns the National Cumulative Distribution of average covered charges for records with the drg_definition of "885 - PSYCHOSES"
+#Retuns Average Cost Information for central Texas, with cumulative distribution partitioned by city. These results are for records with drg_definition of '885 - PSYCHOSES'
+
 cume_df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
-"SELECT STATE, SUM_COVERAGE, cume_dist()
-OVER (ORDER BY SUM_COVERAGE) as Cume_Dist
-FROM (SELECT provider_state as STATE, sum(average_covered_charges) as SUM_COVERAGE
+"SELECT provider_name, provider_city, average_total_payments, cume_dist()
+OVER (PARTITION BY drg_definition, provider_state, provider_city ORDER BY average_total_payments) as cume_dist
 FROM hospital
 WHERE drg_definition = \'885 - PSYCHOSES\'
-GROUP BY provider_state)
-ORDER BY cume_dist desc
+AND provider_state = \'TX\'
+AND provider_city IN (\'AUSTIN\', \'DALLAS\', \'SAN ANTONIO\', \'ROUND ROCK\', \'KILEEN\', \'FORT HOOD\',\'COLLEGE STATION\',\'WACO\')
 "')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_jac5699', PASS='orcl_jac5699', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); kable(head(cume_df))
--- 01 SQL Crosstabs/rank.R
+++ 01 SQL Crosstabs/rank.R
@@ -1,9 +1,10 @@
-#Return Crosstab of Highest number of discharges and its associated DRG_Definition per city. 
+#Returns the lowest average total payments for "Chest Pain" hospital visits in the central Texas area, and ranks them by affordability. 
 
 rank_df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
-"SELECT * FROM (SELECT drg_definition, provider_city, total_discharges, rank() 
-OVER (PARTITION BY provider_city order by total_discharges desc) as DISCHARGE_RANK
-FROM hospital) 
-WHERE DISCHARGE_RANK = 1
-ORDER BY 3 desc"
+"SELECT provider_name, provider_city, average_total_payments, rank()
+OVER (PARTITION BY drg_definition, provider_state ORDER BY average_total_payments) as AFFORDABILITY_RANK
+FROM hospital
+WHERE drg_definition = \'313 - CHEST PAIN\'
+AND provider_state = \'TX\'
+AND provider_city IN (\'AUSTIN\', \'DALLAS\', \'SAN ANTONIO\', \'ROUND ROCK\', \'KILEEN\', \'FORT HOOD\',\'COLLEGE STATION\',\'WACO\')"
 ')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_jac5699', PASS='orcl_jac5699', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); kable(head(rank_df))
